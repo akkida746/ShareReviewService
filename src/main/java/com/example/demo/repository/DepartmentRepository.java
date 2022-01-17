afd99afa904f27e0b9ecb48e76be7a72c0edd532
Akki <akkida746@gmail.com>
2022-01-17 23:50:20 +0000

    changes

---
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.reactive.examples.model.Department;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface DepartmentRepository extends ReactiveCrudRepository<Department,Integer> {
+    Mono<Department> findByUserId(Integer userId);
+}
